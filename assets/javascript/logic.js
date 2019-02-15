a4db127aedeade5b850bb290f20e6a3b0fa2616c
Austin Hickox <austinhickox@yahoo.com>
2019-02-14 21:13:44 -0500

    changed button placement, and added greying out/disabling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -36,6 +36,33 @@ $(document).ready(function () {
         button7State = "empty";
         button8State = "empty";
         button9State = "empty";
+        var element1 = document.getElementById("button1")
+        element1.classList.remove("disabled", "btn-secondary");
+        element1.classList.add("btn-info");
+        var element2 = document.getElementById("button2")
+        element2.classList.remove("disabled", "btn-secondary");
+        element2.classList.add("btn-info");
+        var element3 = document.getElementById("button3")
+        element3.classList.remove("disabled", "btn-secondary");
+        element3.classList.add("btn-info");
+        var element4 = document.getElementById("button4")
+        element4.classList.remove("disabled", "btn-secondary");
+        element4.classList.add("btn-info");
+        var element5 = document.getElementById("button5")
+        element5.classList.remove("disabled", "btn-secondary");
+        element5.classList.add("btn-info");
+        var element6 = document.getElementById("button6")
+        element6.classList.remove("disabled", "btn-secondary");
+        element6.classList.add("btn-info");
+        var element7 = document.getElementById("button7")
+        element7.classList.remove("disabled", "btn-secondary");
+        element7.classList.add("btn-info");
+        var element8 = document.getElementById("button8")
+        element8.classList.remove("disabled", "btn-secondary");
+        element8.classList.add("btn-info");
+        var element9 = document.getElementById("button9")
+        element9.classList.remove("disabled", "btn-secondary");
+        element9.classList.add("btn-info");
         document.getElementById("square1").innerHTML = "";
         document.getElementById("square2").innerHTML = "";
         document.getElementById("square3").innerHTML = "";
@@ -148,6 +175,10 @@ $(document).ready(function () {
         }
     }
 
+    function errorAlert() {
+        document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+    }
+
 
     //button click listeners
 
@@ -155,9 +186,12 @@ $(document).ready(function () {
     $("#button1").click(function () {
         if (button1State == "empty") {
             button1State = currentTurn;
+            var element = document.getElementById("button1")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square1").innerHTML = button1State;
             indicateSquare(1);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
 
     });
 
@@ -165,72 +199,96 @@ $(document).ready(function () {
     $("#button2").click(function () {
         if (button2State == "empty") {
             button2State = currentTurn;
+            var element = document.getElementById("button2")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square2").innerHTML = button2State;
             indicateSquare(2);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
     });
 
     //Button 3 click
     $("#button3").click(function () {
         if (button3State == "empty") {
             button3State = currentTurn;
+            var element = document.getElementById("button3")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square3").innerHTML = button3State;
             indicateSquare(3);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
     });
 
     //Button 4 click
     $("#button4").click(function () {
         if (button4State == "empty") {
             button4State = currentTurn;
+            var element = document.getElementById("button4")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square4").innerHTML = button4State;
             indicateSquare(4);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
     });
 
     //Button 5 click
     $("#button5").click(function () {
         if (button5State == "empty") {
             button5State = currentTurn;
+            var element = document.getElementById("button5")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square5").innerHTML = button5State;
             indicateSquare(5);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
     });
 
     //Button 6 click
     $("#button6").click(function () {
         if (button6State == "empty") {
             button6State = currentTurn;
+            var element = document.getElementById("button6")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square6").innerHTML = button6State;
             indicateSquare(6);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
     });
 
     //Button 7 click
     $("#button7").click(function () {
         if (button7State == "empty") {
             button7State = currentTurn;
+            var element = document.getElementById("button7")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square7").innerHTML = button7State;
             indicateSquare(7);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
     });
 
     //Button 8 click
     $("#button8").click(function () {
         if (button8State == "empty") {
             button8State = currentTurn;
+            var element = document.getElementById("button8")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square8").innerHTML = button8State;
             indicateSquare(8);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
     });
 
     //Button 9 click
     $("#button9").click(function () {
         if (button9State == "empty") {
             button9State = currentTurn;
+            var element = document.getElementById("button9")
+            element.classList.remove("btn-info");
+            element.classList.add("disabled", "btn-secondary");
             document.getElementById("square9").innerHTML = button9State;
             indicateSquare(9);
-        } else { document.getElementById("alertDiv").innerHTML = "<div class='alert alert-info alert-dismissible fade show' role='alert'> Please click an empty square <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>"}
+        } else {errorAlert()}
     });
 
     gameStart();
